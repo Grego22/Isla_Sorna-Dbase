3e7e320c4bf9c645480cae663be65c592316a73d
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-08 14:08:02 -0400

    fixing errors

--- server.js
+++ server.js
@@ -4,49 +4,51 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
-app.engine('mst', mustacheExpress())
+app.engine('mustache', mustacheExpress())
 app.set('views', './views')
-app.set('view engine', 'mst')
+app.set('view engine', 'mustache')
 // define a public folder
 app.use(express.static('public'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extend:false}))
 
-
+app.get('/', (req, res) => {
+  res.render('index',myDinos[1]);
+})
 
 let myDinos = [
   {
     id:1,
     name: 'Triceratops',
     colors: ['blue', 'white'],
-    diet: 'Herbivore'
-    weight: 11000,
+    diet: 'Herbivore',
+    weight: 11000
   },{
     id:2,
     name: 'Velociraptor',
     colors: ['gold', 'green', 'white']
     diet: 'Carnivore'
-    weight: 30,
+    weight: 30
   },
   {
     id:3,
     name: 'T-Rex',
     colors: ['orange', 'white']
-    diet: 'Carnivore'
-    weight: 20000,
+    diet: 'Carnivore',
+    weight: 20000
   },
   {
     id:4,
    name: 'Pachycephalosaurus',
    colors: ['gold', 'silver', 'red'],
-   diet: 'herbivore'
-   weight: 1000,
+   diet: 'herbivore',
+   weight: 1000
  }
 ]
-console.log(myDions.name);
+console.log(myDinos)
 
 
 app.listen(8888, () => {
-  console.log('We are rocking on 3000');
+  console.log('Must go faster!')
 })
