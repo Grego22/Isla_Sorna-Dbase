50d417dc382fd5083dc24b10fb66fb5cdb2d3388
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-08 14:02:11 -0400

    test run

--- server.js
+++ server.js
@@ -12,3 +12,41 @@ app.use(express.static('public'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extend:false}))
+
+
+
+let myDinos = [
+  {
+    id:1,
+    name: 'Triceratops',
+    colors: ['blue', 'white'],
+    diet: 'Herbivore'
+    weight: 11000,
+  },{
+    id:2,
+    name: 'Velociraptor',
+    colors: ['gold', 'green', 'white']
+    diet: 'Carnivore'
+    weight: 30,
+  },
+  {
+    id:3,
+    name: 'T-Rex',
+    colors: ['orange', 'white']
+    diet: 'Carnivore'
+    weight: 20000,
+  },
+  {
+    id:4,
+   name: 'Pachycephalosaurus',
+   colors: ['gold', 'silver', 'red'],
+   diet: 'herbivore'
+   weight: 1000,
+ }
+]
+console.log(myDions.name);
+
+
+app.listen(8888, () => {
+  console.log('We are rocking on 3000');
+})
